83ed6be6ef84e58967c0f0daac23e8fef6612c4f
Andrew Kruse <Sessional@users.noreply.github.com>
2023-10-05 17:49:51 -0500

    clean up users some more

--- Api/Users/UserContracts.cs
+++ Api/Users/UserContracts.cs
@@ -2,14 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LonelyVale.Api.Users;
 
-public record GetUserResponse
-{
-    public long Id { get; set; }
-    public required string Auth0Id { get; set; }
-}
+public record GetUserResponse(long Id, string Auth0Id);
 
-public record GetUserRequest
-{
-    public long? Id { get; set; }
-    public string? Auth0Id { get; set; }
-}
+public record GetUserRequest(long? Id, string? Auth0Id);
+
+public record CreateUserRequest(string Auth0Id);
+
+public record CreateUserResponse(long Id, string Auth0Id);
